9c7c8fcd2dda481cd08eb5385d5843035946c053
James Tedesco <jamestedesco802@gmail.com>
2020-09-22 10:30:27 -0400

    Add passthrough so data can be accessed from tld
    Also deleted unused chart json for cleanliness

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,12 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("assets");
 
+
     eleventyConfig.addPassthroughCopy({
         "node_modules/sal.js/dist/sal.js": "assets/scripts/sal.js",
-        "node_modules/sal.js/dist/sal.css": "assets/styles/sal.css"
+        "node_modules/sal.js/dist/sal.css": "assets/styles/sal.css",
+        "assets/tld-links/General-Election-NSLVE-Data.pdf": "General-Election-NSLVE-Data.pdf",
+        "assets/tld-links/Midterm-Election-NSLVE-Data.pdf": "Midterm-Election-NSLVE-Data.pdf"
     });
 
     return {
