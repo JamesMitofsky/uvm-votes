5bcce1e6c8ad3b5c2373d7827c51e11f92bc06f2
James Tedesco <jamestedesco802@gmail.com>
2020-09-22 10:41:01 -0400

    Shorten tld links

--- .eleventy.js
+++ .eleventy.js
@@ -6,8 +6,8 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({
         "node_modules/sal.js/dist/sal.js": "assets/scripts/sal.js",
         "node_modules/sal.js/dist/sal.css": "assets/styles/sal.css",
-        "assets/tld-links/General-Election-NSLVE-Data.pdf": "General-Election-NSLVE-Data.pdf",
-        "assets/tld-links/Midterm-Election-NSLVE-Data.pdf": "Midterm-Election-NSLVE-Data.pdf"
+        "assets/tld-links/General-Election-NSLVE-Data.pdf": "General-Election.pdf",
+        "assets/tld-links/Midterm-Election-NSLVE-Data.pdf": "Midterm-Election.pdf"
     });
 
     return {
