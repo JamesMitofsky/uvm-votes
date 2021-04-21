57e93dd31061b80fd2b693b3604f9cbf54b1797b
James Tedesco <jamestedesco802@gmail.com>
2021-04-21 12:43:12 -0400

    Remove RSS plugin from config file

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,10 @@
 const pluginDate = require("eleventy-plugin-date");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 
 module.exports = function (eleventyConfig) {
 
     // add text filter plugin from npm
     eleventyConfig.addPlugin(pluginDate);
-    // rss feed builder
-    eleventyConfig.addPlugin(pluginRss);
-
-
 
     // netlify CMS management
     eleventyConfig.addPassthroughCopy("admin");
