cae8f42a7601cecb0bbedf836f67205f7bb09217
James Mitofsky <JamesMitofsky@gmail.com>
2023-03-03 23:25:07 +0100

    Remove lazy loading image plugin to unblock Netlify builds

--- .eleventy.js
+++ .eleventy.js
@@ -1,38 +1,32 @@
 const pluginDate = require("eleventy-plugin-date");
-const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
 
 module.exports = function (eleventyConfig) {
+  // add text filter plugin from npm
+  eleventyConfig.addPlugin(pluginDate);
 
-    // add text filter plugin from npm
-    eleventyConfig.addPlugin(pluginDate);
-    // lazy load images
-    eleventyConfig.addPlugin(lazyImagesPlugin);
+  // netlify CMS management
+  eleventyConfig.addPassthroughCopy("admin");
+  // image assets
+  eleventyConfig.addPassthroughCopy("assets");
+  // top level links access
+  eleventyConfig.addPassthroughCopy({ "assets/tld-links": "." });
 
-    // netlify CMS management
-    eleventyConfig.addPassthroughCopy("admin");
-    // image assets
-    eleventyConfig.addPassthroughCopy("assets")
-    // top level links access
-    eleventyConfig.addPassthroughCopy({ "assets/tld-links": "." })
+  // add animation features
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/sal.js/dist/sal.js": "assets/scripts/sal.js",
+    "node_modules/sal.js/dist/sal.css": "assets/styles/sal.css",
+  });
 
-    // add animation features
-    eleventyConfig.addPassthroughCopy({
-        "node_modules/sal.js/dist/sal.js": "assets/scripts/sal.js",
-        "node_modules/sal.js/dist/sal.css": "assets/styles/sal.css"
-    });
+  // Filter source file names using a glob
+  eleventyConfig.addCollection("Blog", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("blog/**");
+  });
 
-    // Filter source file names using a glob
-    eleventyConfig.addCollection("Blog", function (collectionApi) {
-        return collectionApi.getFilteredByGlob("blog/**");
-    });
-
-    return {
-
-        dir: {
-            // ⚠️ These values are both relative to your input directory.
-            includes: "assets",
-            layouts: "_layouts"
-        }
-
-    }
+  return {
+    dir: {
+      // ⚠️ These values are both relative to your input directory.
+      includes: "assets",
+      layouts: "_layouts",
+    },
+  };
 };
